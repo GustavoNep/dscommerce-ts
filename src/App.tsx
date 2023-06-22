2d72c8a5fce70ab2236af60bd7b3d0e6d0687285
Gustavo Nep <gustavo._12@hotmail.com>
2023-06-22 13:11:11 -0300

    feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
+import ProductDetails from './routes/ProductDetails'
+
 
 function App() {
   return (
     <>
-      <h1>DsComerce</h1>
+      <ProductDetails />
     </>
   )
 }
---
+++ src/routes/ProductDetails/index.tsx
@@ -0,0 +1,11 @@
+import HeaderClient from "../../components/HeaderClient";
+import ProductDetailsCard from "../../components/ProductDetailsCard";
+
+export default function ProductDetails() {
+    return (
+        <>
+            <HeaderClient />
+            <ProductDetailsCard />
+        </>
+    )
+}
