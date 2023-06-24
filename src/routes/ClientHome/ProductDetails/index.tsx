af2445f7d85f5930a33170871d1f8c1a6693d145
Gustavo Nep <gustavo._12@hotmail.com>
2023-06-24 11:10:20 -0300

    axios req products catalo

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -10,14 +10,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function ProductDetails() {
-  //const params = useParams();
+  const params = useParams();
 
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products/2")
+    axios.get(`http://localhost:8080/products/${params.productId}`)
       .then(response => {
-        console.log(response.data);
         setProduct(response.data)
       })
   }, [])
