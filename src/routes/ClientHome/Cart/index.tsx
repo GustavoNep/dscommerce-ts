cdecb8f9d5d6c2feb9a7ae293bc82ec0cc40dc10
Gustavo Nep <gustavo._12@hotmail.com>
2023-06-24 19:06:22 -0300

    localstorage service layers

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,33 +1,44 @@
 import "./styles.css";
+import { useEffect, useState } from "react";
+import * as cartService from "../../../services/cart-service";
+import { OrderDTO, OrderItemDTO } from "../../../models/order";
 
-const cart = {
-  items: [
-    {
-      productId: 4,
-      quantity: 1,
-      name: "PC Gamer",
-      price: 1200,
-      imgUrl:
-        "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg",
-    },
-    {
-      productId: 5,
-      quantity: 2,
-      name: "Rails for Dummies",
-      price: 100.99,
-      imgUrl:
-        "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg",
-    },
-  ],
-};
+const item1: OrderItemDTO = new OrderItemDTO(
+  4,
+  1,
+  "PC GAMER",
+  1200,
+  "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jp"
+);
+
+const item2: OrderItemDTO = new OrderItemDTO(
+  5,
+  2,
+  "Rails for Dummies",
+  100.99,
+  "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"
+);
+
+const cart : OrderDTO = new OrderDTO();
 
 export default function Cart() {
+  useEffect(() => {
+    
+    cart.items.push(item1);
+    cart.items.push(item2);
+
+    cartService.saveCart(cart);
+  }, []);
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
         <div className="dsc-card dsc-mb20">
           {cart.items.map((x) => (
-            <div key={x.productId} className="dsc-cart-item-container dsc-line-bottom">
+            <div
+              key={x.productId}
+              className="dsc-cart-item-container dsc-line-bottom"
+            >
               <div className="dsc-cart-item-left">
                 <img src={x.imgUrl} alt={x.name} />
                 <div className="dsc-cart-item-description">
@@ -39,7 +50,9 @@ export default function Cart() {
                   </div>
                 </div>
               </div>
-              <div className="dsc-cart-item-right">R$ {(x.price * x.quantity).toFixed(2)}</div>
+              <div className="dsc-cart-item-right">
+                R$ {(x.price * x.quantity).toFixed(2)}
+              </div>
             </div>
           ))}
 
