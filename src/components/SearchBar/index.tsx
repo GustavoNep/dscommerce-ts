1a0fb9f73f381ba048d20926397bd3074743b69b
Gustavo Nep <gustavo._12@hotmail.com>
2023-07-07 20:57:46 -0300

    feat: filter by params

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./styles.css"
 
-export default function SearchBar() {
+type Props = {
+  onSearch: Function;
+}
+
+export default function SearchBar({onSearch} : Props) {
+
+  const [text, setText] = useState("");
+
+  function handleChange(event: any) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault(); // não recarregue página
+    onSearch(text);
+  }
+
   return (
-    <form className="dsc-search-bar">
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
       <button type="submit">🔎︎</button>
-      <input type="text" placeholder="Nome do produto" />
+      <input value={text} type="text" placeholder="Nome do produto" onChange={handleChange}/>
       <button type="reset">🗙</button>
     </form>
   );
