3c77ec77235dac67668f31267a991f5a4a419772
Gustavo Nep <gustavo._12@hotmail.com>
2023-08-17 15:16:12 -0300

    admin page layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Cart from "./routes/ClientHome/Cart";
 import { useState } from "react";
 import { ContextCartCount } from "./utils/context-cart";
 import Login from "./routes/ClientHome/Login";
+import Admin from "./routes/ClientHome/Admin";
+import AdminHome from "./routes/ClientHome/Admin/AdminHome";
 
 function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -24,7 +26,12 @@ function App() {
             <Route path="cart" element={<Cart />} />
             <Route path="login" element={<Login />} />
           </Route>
+          <Route path="/admin/" element={<Admin />}>
+            <Route index element={<AdminHome />}/>
+
+          </Route>
           <Route path="*" element={<Navigate to="/" />} />
+
         </Routes>
       </BrowserRouter>
     </ContextCartCount.Provider>
---
+++ src/components/HeaderAdmin/index.tsx
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+
+export default function HeaderAdmin() {
+  return (
+    <header className="dsc-header-admin">
+      <nav className="dsc-container">
+        <h1>DSC Admin</h1>
+        <div className="dsc-navbar-right">
+          <div className="dsc-menu-items-container">
+            <div className="dsc-menu-item">
+              <p>Início</p>
+            </div>
+            <div className="dsc-menu-item">
+              <p className="dsc-menu-item-active">Produtos</p>
+            </div>
+          </div>
+          <div className="dsc-logged-user">
+            <p>Maria Silva</p>
+            <a href="#">Sair</a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
