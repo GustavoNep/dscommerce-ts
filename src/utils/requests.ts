740ab3b9705a9dfa0fb42bd0ae13359df0a7f018
Gustavo Nep <gustavo._12@hotmail.com>
2023-08-19 16:20:20 -0300

    redirect outside react component

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,7 +1,49 @@
 import axios, { AxiosRequestConfig } from "axios";
-
+import * as authService from '../services/auth-service'
 import { BASE_URL } from "./system";
+import { history } from "./history";
 
 export function requestBackend(config: AxiosRequestConfig) {
-    return axios({...config, baseURL: BASE_URL});
+
+    const headers = config.withCredentials
+        ? {
+            ...config.headers,
+            Authorization: "Bearer " + authService.getAccessToken()
+        }
+        : config.headers;
+       
+
+    return axios({ ...config, baseURL: BASE_URL, headers });
 }
+
+// REQUEST INTERCEPTOR
+
+axios.interceptors.request.use(
+    function(config) {
+        // DO SOMETHING BEFORE REQUEST IS SENT
+        return config;
+    }, 
+    function(error) {
+        return Promise.reject(error);
+    }
+);
+
+// RESPONSE INTERCEPTOR
+
+axios.interceptors.response.use(
+    function(response) {
+        // DO SOMETHING WITH RESPONSE DATA IF STATUS IS
+        return response;
+    },
+    function (error) {
+        if (error.response.status === 401) {
+           history.push("/login")
+        }
+        if (error.response.status === 403) {
+            history.push("/catalog")
+         }
+        return Promise.reject(error);
+    }
+)
+
+
--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = "http://localhost:8080";
 export const CART_KEY = "com.devsuperior.dscommerce/Cart";
 export const TOKEN_KEY = "com.devsuperior.dscommerce/Token";
-export const CLIENT_ID = "myclientid";
+export const CLIENT_ID = "myclientid"; // definidas no backend
 export const CLIENT_SECRET = "myclientsecret";
 
