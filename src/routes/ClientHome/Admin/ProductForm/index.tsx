bf9006605a72ca77fc7190c1bd2291770ab1213c
Gustavo Nep <gustavo._12@hotmail.com>
2023-09-15 22:02:14 -0300

    feat: using params to search id product

--- src/routes/ClientHome/Admin/ProductForm/index.tsx
+++ src/routes/ClientHome/Admin/ProductForm/index.tsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./styles.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FormInput from "../../../../components/FormInput";
 import * as forms from "../../../../utils/forms";
+import * as productService from "../../../../services/product-service";
 
 export default function ProductForm() {
+  const params = useParams();
+
+  const isEditing = params.productId !== "create";
+
   const [formData, setFormData] = useState<any>({
     name: {
       value: "",
@@ -28,6 +33,14 @@ export default function ProductForm() {
       placeholder: "Imagem",
     },
   });
+
+  useEffect(() => {
+    if (isEditing) {
+      productService.findById(Number(params.productId)).then((response) => {
+        console.log(response.data);
+      });
+    }
+  }, []);
 
   function handleInputChange(event: any) {
     const value = event.target.value;
