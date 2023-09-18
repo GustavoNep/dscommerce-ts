8eed067bad848b0aa192da5cbc9b41efd02bfbc5
Gustavo Nep <gustavo._12@hotmail.com>
2023-09-18 15:11:10 -0300

    feat: forms validate function

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,5 +1,5 @@
 export function update(inputs: any, name: string, newValue: any) {
-    return { ...inputs, [name]: { ...inputs[name], value: newValue} }
+    return { ...inputs, [name]: { ...inputs[name], value: newValue } }
 }
 
 export function toValues(inputs: any) {
@@ -13,8 +13,18 @@ export function toValues(inputs: any) {
 export function updateAll(inputs: any, newValues: any) {
     const newInputs: any = {};
     for (var name in inputs) {
-        newInputs[name] = {...inputs[name], value: newValues[name]};
+        newInputs[name] = { ...inputs[name], value: newValues[name] };
     }
     return newInputs;
 
+}
+
+export function validate(inputs: any, name: string) {
+
+    if (!inputs[name].validation) {
+        return inputs;
+    }
+
+    const isInvalid = !inputs[name].validation(inputs[name].value)
+    return { ...inputs, [name]: { ...inputs[name], invalid: isInvalid.toString() } }
 }
