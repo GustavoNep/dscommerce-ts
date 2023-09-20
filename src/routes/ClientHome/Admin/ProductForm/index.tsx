254ce4b90854cfdb5be534e4e7b306a977b2a46c
Gustavo Nep <gustavo._12@hotmail.com>
2023-09-20 12:16:47 -0300

    fix: prevent empty blank submit forms dirty

--- src/routes/ClientHome/Admin/ProductForm/index.tsx
+++ src/routes/ClientHome/Admin/ProductForm/index.tsx
@@ -99,6 +99,12 @@ export default function ProductForm() {
 
   function handleSubmit(event: any) {
     event.preventDefault();
+
+    const formDataValidated = forms.dirtyAndValidateAll(formData);
+    if (forms.hasAnyInvalid(formDataValidated)) {
+      setFormData(formDataValidated);
+      return;
+    }
     console.log(forms.toValues(formData));
   }
   return (
